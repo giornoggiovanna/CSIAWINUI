b452173b41647db7b15a437738497dd4a8612819
Hayden Christopher Zamora <hayzam1022@staden.gotheborg.net>
2022-12-29 17:54:24 +0100

    Renaming some of the classes...

---
+++ TimesUp/ViewModels/ToDoTasksViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using TimesUp.Database;
+
+namespace TimesUp.ViewModels
+{
+    public class ToDoTasksViewModel
+    {
+        public ObservableCollection<ToDoTaskItemViewModel> Tasks { get; set; } = new();
+
+        public ToDoTasksViewModel()
+        {
+            LoadTasks();
+        }
+
+        public void LoadTasks()
+        {
+            var dbTasks = DataAccess.GetToDoTasks();
+
+            Tasks.Clear();
+
+            foreach (var dbTask in dbTasks)
+            {
+                Tasks.Add(new ToDoTaskItemViewModel
+                {
+                    Id = dbTask.Id,
+                    Name = dbTask.Name,
+                    Description = dbTask.Description,
+                    CurrentEffort = dbTask.CurrentEffort,
+                    ExpectedEffort = dbTask.ExpectedEffort
+                });
+            }
+        }
+    }
+
+    public class ToDoTaskItemViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ExpectedEffort { get; set; }
+        public int CurrentEffort { get; set; }
+        public int RemainingEffort { get => ExpectedEffort - CurrentEffort; }
+    }
+}
